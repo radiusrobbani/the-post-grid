4add5ff8030011f8c481d33fe45fc8a8b7b5a7d4
Habib Rahaman <habibjh88@gmail.com>
2021-10-14 16:14:46 +0600

    init

--- lib/views/settings/post-source.php
+++ lib/views/settings/post-source.php
@@ -1,29 +1,30 @@
 <?php
-echo rtTPG()->rtFieldGenerator(rtTPG()->rtTPGPostType());
+
+echo rtTPG()->rtFieldGenerator( rtTPG()->rtTPGPostType() );
 $sHtml = null;
-$sHtml .= '<div class="field-holder">';
-    $sHtml .= '<div class="field-label">Common filters</div>';
-    $sHtml .= '<div class="field">';
-        $sHtml .=rtTPG()->rtFieldGenerator(rtTPG()->rtTPGCommonFilterFields(), true);
-    $sHtml .= '</div>';
+$sHtml .= '<div class="field-holder rt-tpg-field-group">';
+$sHtml .= '<div class="field-label">Common filters</div>';
+$sHtml .= '<div class="field">';
+$sHtml .= rtTPG()->rtFieldGenerator( rtTPG()->rtTPGCommonFilterFields(), true );
+$sHtml .= '</div>';
 $sHtml .= '</div>';
 
 echo $sHtml;
 
 ?>
 
-<div class='rt-tpg-filter-container'>
-    <?php echo rtTPG()->rtFieldGenerator(rtTPG()->rtTPAdvanceFilters()); ?>
+<div class='rt-tpg-filter-container rt-tpg-field-group'>
+	<?php echo rtTPG()->rtFieldGenerator( rtTPG()->rtTPAdvanceFilters() ); ?>
     <div class="rt-tpg-filter-holder">
-        <?php
-            $html = null;
-            $pt = get_post_meta($post->ID, 'tpg_post_type', true);
-            $advFilters = rtTPG()->rtTPAdvanceFilters();
-            echo $html;
-        ?>
+		<?php
+		$html       = null;
+		$pt         = get_post_meta( $post->ID, 'tpg_post_type', true );
+		$advFilters = rtTPG()->rtTPAdvanceFilters();
+		echo $html;
+		?>
     </div>
 </div>
 
-<?php
-echo rtTPG()->rtFieldGenerator(rtTPG()->stickySettings(), true);
-?>
+<div class="rt-tpg-field-group">
+	<?php echo rtTPG()->rtFieldGenerator( rtTPG()->stickySettings(), true ); ?>
+</div>
