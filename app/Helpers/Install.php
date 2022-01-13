8005b1b12e38e44d8282844487725794518499d9
aareyad <aliakbar.reyad@gmail.com>
2022-01-13 09:41:12 +0600

    Fixed active error issue

--- app/Helpers/Install.php
+++ app/Helpers/Install.php
@@ -6,14 +6,14 @@ namespace RT\ThePostGrid\Helpers;
 
 class Install {
 	function activate() {
-		$this->insertDefaultData();
+		self::insertDefaultData();
 	}
 
 	function deactivate() {
 
 	}
 
-	private function insertDefaultData() {
+	public static function insertDefaultData() {
 		update_option( rtTPG()->options['installed_version'], RT_THE_POST_GRID_VERSION );
 		if ( ! get_option( rtTPG()->options['settings'] ) ) {
 			update_option( rtTPG()->options['settings'], rtTPG()->defaultSettings );
