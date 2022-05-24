a5791a35a40a224e033d90f4e2d1d5176cccab02
Habib Rahaman <habibjh88@gmail.com>
2022-05-24 12:40:32 +0600

    redme file updated

--- the-post-grid.php
+++ the-post-grid.php
@@ -5,24 +5,19 @@
  * Plugin URI: http://demo.radiustheme.com/wordpress/plugins/the-post-grid/
  * Description: Fast & Easy way to display WordPress post in Grid, List & Isotope view ( filter by category, tag, author..)  without a single line of coding.
  * Author: RadiusTheme
- * Version: 4.0.3
+ * Version: 4.0.4
  * Text Domain: the-post-grid
  * Domain Path: /languages
  * Author URI: https://radiustheme.com/
  */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-$plugin_data = get_file_data( __FILE__,
-	[
-		'name'    => 'Plugin Name',
-		'version' => 'Version',
-		'author'  => 'Author',
-	],
-	false );
-define( 'RT_THE_POST_GRID_VERSION', $plugin_data['version'] );
-define( 'RT_THE_POST_GRID_AUTHOR', $plugin_data['author'] );
-define( 'RT_THE_POST_GRID_NAME', $plugin_data['name'] );
+
+define( 'RT_THE_POST_GRID_VERSION', '4.0.4' );
+define( 'RT_THE_POST_GRID_AUTHOR', 'RadiusTheme' );
+define( 'RT_THE_POST_GRID_NAME', 'The Post Grid' );
 define( 'RT_THE_POST_GRID_PLUGIN_FILE', __FILE__ );
 define( 'RT_THE_POST_GRID_PLUGIN_PATH', dirname( __FILE__ ) );
 define( 'RT_THE_POST_GRID_PLUGIN_ACTIVE_FILE_NAME', plugin_basename( __FILE__ ) );
@@ -33,4 +28,3 @@ define( 'RT_THE_POST_GRID_LANGUAGE_PATH', dirname( plugin_basename( __FILE__ ) )
 if ( ! class_exists( 'rtTPG' ) ) {
 	require_once 'app/RtTpg.php';
 }
-
